555293191a2a306a5c67e420e26babdf03bfddec
rick <rick@d71426e3-2fed-e111-98e1-0030489379b8>
2012-08-25 22:19:03 +0000

    Add TextureConvert project and related FileFormats code.

---
+++ trunk/Gibbed.DarkSouls.TextureConvert/Program.cs
@@ -0,0 +1,141 @@
+﻿/* Copyright (c) 2012 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Gibbed.DarkSouls.FileFormats;
+using Gibbed.IO;
+using NDesk.Options;
+
+namespace Gibbed.DarkSouls.TextureConvert
+{
+    public class Program
+    {
+        private static string GetExecutableName()
+        {
+            return Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+        }
+
+        public static void Main(string[] args)
+        {
+            bool showHelp = false;
+            bool overwriteFiles = false;
+            bool verbose = true;
+            Endian endian = Endian.Little;
+
+            var options = new OptionSet()
+            {
+                {
+                    "l|little-endian",
+                    "treat file as little-endian (default)",
+                    v => endian = v != null ? Endian.Little : endian
+                    },
+                {
+                    "b|big-endian",
+                    "treat file as big-endian",
+                    v => endian = v != null ? Endian.Big : endian
+                    },
+                {
+                    "o|overwrite",
+                    "overwrite existing files",
+                    v => overwriteFiles = v != null
+                    },
+                {
+                    "v|verbose",
+                    "be verbose",
+                    v => verbose = v != null
+                    },
+                {
+                    "h|help",
+                    "show this message and exit",
+                    v => showHelp = v != null
+                    },
+            };
+
+            List<string> extras;
+
+            try
+            {
+                extras = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("{0}: ", GetExecutableName());
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `{0} --help' for more information.", GetExecutableName());
+                return;
+            }
+
+            if (extras.Count < 1 ||
+                extras.Count > 2 ||
+                showHelp == true)
+            {
+                Console.WriteLine("Usage: {0} [OPTIONS]+ input_file.tpf [output_dir]", GetExecutableName());
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                options.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            string inputPath = extras[0];
+            string outputPath = extras.Count > 1 ? extras[1] : Path.ChangeExtension(inputPath, null) + "_unpack";
+
+            var tpf = new TexturePackageFile();
+            using (var input = File.OpenRead(inputPath))
+            {
+                tpf.Deserialize(input, endian);
+            }
+
+            foreach (var texture in tpf.Textures)
+            {
+                var entryName = texture.Name;
+                entryName = entryName.Replace('/', '\\');
+
+                var formatName = texture.Format.ToString();
+                if (formatName.StartsWith("Unknown") == true)
+                {
+                    entryName += " [" + formatName + "]";
+                }
+
+                var entryPath = Path.Combine(outputPath, Path.ChangeExtension(entryName, ".dds"));
+
+                if (overwriteFiles == false &&
+                    File.Exists(entryPath) == true)
+                {
+                    continue;
+                }
+
+                var parentPath = Path.GetDirectoryName(entryPath);
+                if (parentPath != null)
+                {
+                    Directory.CreateDirectory(parentPath);
+                }
+
+                using (var output = File.Create(entryPath))
+                {
+                    output.WriteBytes(texture.Data);
+                }
+            }
+        }
+    }
+}
